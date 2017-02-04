d020d004ee761ec180c117ce7070f12a040c7d11
Chen Zhang <morning1319@gmail.com>
2017-02-04 15:26:05 -0800

    recovery

--- CarND-Behavioral-Cloning/model.py
+++ CarND-Behavioral-Cloning/model.py
@@ -3,12 +3,32 @@ import numpy as np
 from scipy.misc import imread
 from random import uniform
 from keras.layers import Input, Activation, Dropout, Dense, Flatten, BatchNormalization 
-from keras.models import Sequential, model_from_json
+from keras.models import Sequential, model_from_json, load_model
 from keras.layers.convolutional import Convolution2D
 from keras.layers.pooling import MaxPooling2D
 from keras.optimizers import RMSprop
 from keras.optimizers import Adam
 from sklearn.model_selection import train_test_split
+
+
+#data_file = '/home/linfeng-zc/Documents/Udacity/CarND-Behavioral-Cloning/data/data/TEST_IMG/driving_log.csv'
+#image_path = '/home/linfeng-zc/Documents/Udacity/CarND-Behavioral-Cloning/data/data/'
+
+#data_file = '/home/linfeng-zc/Documents/Udacity/CarND-Behavioral-Cloning/data/example_data/driving_log.csv'
+#image_path = '/home/linfeng-zc/Documents/Udacity/CarND-Behavioral-Cloning/data/data/'
+
+data_file = '/home/linfeng-zc/Documents/Udacity/CarND-Behavioral-Cloning/data/track_data/driving_log.csv'
+image_path = ''
+
+NUM_EPOCHS = 10
+BATCH_SIZE = 32
+
+ROI_ROW = 60
+
+ROWS = 20
+COLS = 64
+CHANNELS = 1
+
 
 # Convert one channel 2D image to 3D image with dimention (rows, cols, 1)
 def toRank3(gray_image):
@@ -18,16 +38,20 @@ def toRank3(gray_image):
 
 # Preprocessing images
 def preprocess(image):
-	# Resize image
-	resized_image = cv2.resize(image, (160, 80))
+	# To YUV
+	img_yuv = cv2.cvtColor(image, cv2.COLOR_BGR2YUV)
 
-	# Take Y channel and normalize image
-	img_yuv = cv2.cvtColor(resized_image, cv2.COLOR_BGR2YUV)
-	img_y = img_yuv[:, :, 1]
-	img_y = np.float32(img_y)
-	#img_y = (img_y - np.mean(img_y)) / np.mean(img_y)
-	img_y = (img_y - 128.0) / 128.0
-	return toRank3(img_y)
+	# Crop ROI and take Y channel
+	img_roi = image[ROI_ROW:, :, 1]
+
+	# Resize image
+	resized_roi = cv2.resize(img_roi, (COLS, ROWS))
+
+	# Normalize image
+	resized_roi = np.float32(resized_roi)
+	resized_roi = (resized_roi - 128.0) / 128.0
+
+	return toRank3(resized_roi)
 
 def get_train_validation_path(data_file, image_path, validation_prob):
 	train_lines = []
@@ -37,7 +61,7 @@ def get_train_validation_path(data_file, image_path, validation_prob):
 			if i == 0:
 				continue
 			prob = uniform(0, 1)
-			if prob >= 0:
+			if prob >= validation_prob:
 				train_lines.append(line.strip())
 			else:
 				validation_lines.append(line.strip())
@@ -68,17 +92,16 @@ def get_generator(lines, image_path, batch_size):
 def get_nvidia_model():
 	model = Sequential()
 	
-	model.add(Convolution2D(24, 5, 5, activation='relu', input_shape=(80, 160, 1)))
-	model.add(MaxPooling2D(pool_size=(2, 2)))
+	model.add(BatchNormalization(mode=2, axis=1, input_shape=(ROWS, COLS, CHANNELS)))
+	model.add(Convolution2D(24, 3, 3, border_mode='valid', activation='relu'))
 
-	model.add(Convolution2D(36, 5, 5, activation='relu'))
-	model.add(MaxPooling2D(pool_size=(2, 2)))
+	model.add(Convolution2D(36, 3, 3, border_mode='valid', activation='relu'))
 
-	model.add(Convolution2D(48, 5, 5, activation='relu'))
-	model.add(MaxPooling2D(pool_size=(2, 2)))
+	model.add(Convolution2D(48, 3, 3, border_mode='valid', activation='relu'))
 
-	model.add(Convolution2D(64, 5, 5, activation='relu'))
-	model.add(MaxPooling2D(pool_size=(2, 2)))
+	model.add(Convolution2D(64, 3, 3, border_mode='valid', activation='relu'))
+
+	model.add(Convolution2D(64, 3, 3, border_mode='valid', activation='relu'))
 
 	model.add(Flatten())
 
@@ -93,7 +116,7 @@ def get_nvidia_model():
 	#model.add(Dropout(0.25))
 	model.add(Dense(1, activation='linear'))
 
-	model.compile(optimizer=Adam(lr=1e-4), loss='mse')
+	model.compile(optimizer=Adam(lr=1e-4), loss='mse', metrics=['accuracy'])
 	return model
 
 def get_model():
@@ -101,9 +124,6 @@ def get_model():
 	weight_init='glorot_normal'
 	opt = RMSprop(lr)
 	loss = 'mean_squared_error'
-	ROWS = 80
-	COLS = 160
-	CHANNELS = 1
 
 	model = Sequential()
 
@@ -132,47 +152,47 @@ def get_model():
 	
 	return model
 
-
-#data_file = '/home/linfeng-zc/Documents/Udacity/CarND-Behavioral-Cloning/data/data/TEST_IMG/driving_log.csv'
-#image_path = '/home/linfeng-zc/Documents/Udacity/CarND-Behavioral-Cloning/data/data/'
-data_file = '/home/linfeng-zc/Documents/Udacity/CarND-Behavioral-Cloning/data/data/driving_log.csv'
-image_path = '/home/linfeng-zc/Documents/Udacity/CarND-Behavioral-Cloning/data/data/'
-num_epoch = 10
-batch_size = 32
-
-[train_lines, validation_lines] = get_train_validation_path(data_file, image_path, 0.2)
-
-model = get_model()
-#model.summary()
-
-#model.fit_generator(get_generator(train_lines, image_path, batch_size),
-#	nb_epoch=num_epoch, samples_per_epoch=len(train_lines), nb_val_samples=len(validation_lines),
-#	validation_data=get_generator(validation_lines, image_path, batch_size))
-
-model.fit_generator(get_generator(train_lines, image_path, batch_size), 
-	nb_epoch=num_epoch, samples_per_epoch=len(train_lines), callbacks=None)
-
-#model.fit(preprocessed_img1[None, :, :, :], np.array([0.0617599]), nb_epoch=num_epoch, batch_size=batch_size, verbose=2)
-img1 = imread(image_path + 'IMG/center_2016_12_01_13_32_43_457.jpg').astype(np.float32)
-img2 = imread(image_path + 'IMG/center_2016_12_01_13_32_49_008.jpg').astype(np.float32)
-preprocessed_img1 = preprocess(img1)
-preprocessed_img2 = preprocess(img2)
+#TODO
+def three_imgs_test():
+	img1 = imread(image_path + 'IMG/center_2016_12_01_13_32_43_457.jpg').astype(np.float32)
+	img2 = imread(image_path + 'IMG/center_2016_12_01_13_32_49_008.jpg').astype(np.float32)
+	preprocessed_img1 = preprocess(img1)
+	preprocessed_img2 = preprocess(img2)
 
 
-cv2.imshow("img", preprocessed_img2[:, :, 0])
-cv2.imshow("preprocessed_img", preprocessed_img1[ :, :, 0])
-cv2.waitKey(0)
+	cv2.imshow("img", preprocessed_img2[:, :, 0])
+	cv2.imshow("preprocessed_img", preprocessed_img1[ :, :, 0])
+	cv2.waitKey(0)
 
-steering_angle1 = float(model.predict(preprocessed_img1[None, :, :, :], batch_size=1))
-steering_angle2 = float(model.predict(preprocessed_img2[None, :, :, :], batch_size=1))
+	steering_angle1 = float(model.predict(preprocessed_img1[None, :, :, :], batch_size=1))
+	steering_angle2 = float(model.predict(preprocessed_img2[None, :, :, :], batch_size=1))
 
-print ('steering angle: ', steering_angle1, steering_angle2)
+	print ('steering angle: ', steering_angle1, steering_angle2)
 
-# serialize model to JSON
-model_json = model.to_json()
-with open("model.json", "w") as json_file:
-    json_file.write(model_json)
-# serialize weights to HDF5
-model.save_weights("model.h5")
+if __name__ == '__main__':
+	[train_lines, validation_lines] = get_train_validation_path(data_file, image_path, 0.2)
 
-print("Saved model to disk")
+	model = get_nvidia_model()
+	#model.summary()
+
+	model.fit_generator(get_generator(train_lines, image_path, BATCH_SIZE),
+		nb_epoch=NUM_EPOCHS, samples_per_epoch=len(train_lines), nb_val_samples=len(validation_lines),
+		validation_data=get_generator(validation_lines, image_path, BATCH_SIZE))
+
+	#model.fit_generator(get_generator(train_lines, image_path, BATCH_SIZE), 
+	#	nb_epoch=NUM_EPOCHS, samples_per_epoch=len(train_lines), callbacks=None)
+
+	#model.fit(preprocessed_img1[None, :, :, :], np.array([0.0617599]), nb_epoch=NUM_EPOCHS, batch_size=BATCH_SIZE, verbose=2)
+
+	'''
+	# serialize model to JSON
+	model_json = model.to_json()
+	with open("model.json", "w") as json_file:
+	    json_file.write(model_json)
+	# serialize weights to HDF5
+	model.save_weights("model.h5")
+
+	print("Saved model to disk")
+	'''
+	
+	model.save('model.h5')
