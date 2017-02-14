e19ce51baacfa53773e80be26276cd1d56d70375
Chen Zhang <morning1319@gmail.com>
2017-02-13 22:57:47 -0800

    add main function, fit_line_quick[based on previous frame result]

--- CarND-Advanced-Lane-Lines/fit_lines.py
+++ CarND-Advanced-Lane-Lines/fit_lines.py
@@ -79,7 +79,8 @@ def fit_line(binary_warped):
 	ploty = np.linspace(0, binary_warped.shape[0]-1, binary_warped.shape[0] )
 	left_fitx = left_fit[0]*ploty**2 + left_fit[1]*ploty + left_fit[2]
 	right_fitx = right_fit[0]*ploty**2 + right_fit[1]*ploty + right_fit[2]
-
+	
+	'''
 	out_img[nonzeroy[left_lane_inds], nonzerox[left_lane_inds]] = [255, 0, 0]
 	out_img[nonzeroy[right_lane_inds], nonzerox[right_lane_inds]] = [0, 0, 255]
 	plt.imshow(out_img)
@@ -87,6 +88,56 @@ def fit_line(binary_warped):
 	plt.plot(right_fitx, ploty, color='yellow')
 	plt.xlim(0, 1280)
 	plt.ylim(720, 0)
+	'''
+	return left_fitx, right_fitx, ploty
+
+def fit_line_quick(binary_warped):
+	# Assume you now have a new warped binary image
+	# from the next frame of video (also called "binary_warped")
+	# It's now much easier to find line pixels!
+	nonzero = binary_warped.nonzero()
+	nonzeroy = np.array(nonzero[0])
+	nonzerox = np.array(nonzero[1])
+	margin = 100
+	left_lane_inds = ((nonzerox > (left_fit[0]*(nonzeroy**2) + left_fit[1]*nonzeroy + left_fit[2] - margin)) & (nonzerox < (left_fit[0]*(nonzeroy**2) + left_fit[1]*nonzeroy + left_fit[2] + margin)))
+	right_lane_inds = ((nonzerox > (right_fit[0]*(nonzeroy**2) + right_fit[1]*nonzeroy + right_fit[2] - margin)) & (nonzerox < (right_fit[0]*(nonzeroy**2) + right_fit[1]*nonzeroy + right_fit[2] + margin)))
+
+	# Again, extract left and right line pixel positions
+	leftx = nonzerox[left_lane_inds]
+	lefty = nonzeroy[left_lane_inds]
+	rightx = nonzerox[right_lane_inds]
+	righty = nonzeroy[right_lane_inds]
+	# Fit a second order polynomial to each
+	left_fit = np.polyfit(lefty, leftx, 2)
+	right_fit = np.polyfit(righty, rightx, 2)
+	# Generate x and y values for plotting
+	ploty = np.linspace(0, binary_warped.shape[0]-1, binary_warped.shape[0] )
+	left_fitx = left_fit[0]*ploty**2 + left_fit[1]*ploty + left_fit[2]
+	right_fitx = right_fit[0]*ploty**2 + right_fit[1]*ploty + right_fit[2]
+
+	return left_fitx, right_fitx, ploty
+
+def warp_pespective(undist, binary_warped, left_fitx, right_fitx, ploty, Minv):
+	# Create an image to draw the lines on
+	warp_zero = np.zeros_like(binary_warped).astype(np.uint8)
+	color_warp = np.dstack((warp_zero, warp_zero, warp_zero))
+
+	# Recast the x and y points into usable format for cv2.fillPoly()
+	pts_left = np.array([np.transpose(np.vstack([left_fitx, ploty]))])
+	pts_right = np.array([np.flipud(np.transpose(np.vstack([right_fitx, ploty])))])
+	pts = np.hstack((pts_left, pts_right))
+
+	# Draw the lane onto the warped blank image
+	cv2.fillPoly(color_warp, np.int_([pts]), (0,255, 0))
+
+	# Warp the blank back to original image space using inverse perspective matrix (Minv)
+	newwarp = cv2.warpPerspective(color_warp, Minv, (undist.shape[1], undist.shape[0]))
+	# Combine the result with the original image
+	result = cv2.addWeighted(undist, 1, newwarp, 0.3, 0)
+
+	return result
+
+
 
 def window_mask(width, height, img_ref, center,level):
 	output = np.zeros_like(img_ref)
---
+++ CarND-Advanced-Lane-Lines/main.py
@@ -0,0 +1,88 @@
+import os.path
+import matplotlib.image as mpimg
+import matplotlib.pyplot as plt
+from moviepy.editor import VideoFileClip
+import glob
+import json
+import cv2
+import numpy as np
+from camera_calibration import *
+from color_gradient_threshold import *
+from fit_lines import *
+
+def calibrate(calib_image_path, calib_json, camera_matrix, dist_coeff):
+	# check if camera parameters exists
+	if os.path.isfile(calib_json):
+		with open(calib_json) as camera_calibration:
+			calib = json.load(camera_calibration)
+			mtx = np.array(calib[camera_matrix])
+			dist = np.array(calib[dist_coeff])
+	else:
+		# Chessboard shape
+		nx = 9
+		ny = 6
+		# Calibrate images
+		calib_images = glob.glob(calib_image_path)
+		ret, mtx, dist, rvecs, tvecs = calibrate_camera(calib_images, (nx, ny))
+
+		if (ret):
+			data = {camera_matrix: mtx.tolist(), dist_coeff: dist.tolist()}
+			with open(calib_json, "w") as f:
+				json.dump(data, f)
+
+	return mtx, dist
+
+def process_image(img):
+
+	# Calibrate camera
+	calib_image_path = "/home/linfeng-zc/Documents/Udacity/CarND-Advanced-Lane-Lines/camera_cal/*"
+	calib_json = 'camera_clibration.json'
+	camera_matrix = 'camera_matrix'
+	dist_coeff = 'dist_coeff'
+	mtx, dist = calibrate(calib_image_path, calib_json, camera_matrix, dist_coeff)
+
+	# Get perspective transform matrix
+	src = np.float32(
+		[[511, 511],
+		 [781, 511],
+		 [438, 564],
+		 [859, 564]])
+
+	dst = np.float32(
+		[[465, 520],
+		 [815, 520],
+		 [465, 620],
+		 [815, 620]])
+
+	M = get_perspective_transform(src, dst)
+	Minv = get_perspective_transform(dst, src)
+
+	# Get binary warped image
+	undist = cv2.undistort(img, mtx, dist)
+	binary_img = pipline(undist)
+	binary_warped = perspective_transform(binary_img, M)
+
+	# Fit line
+	left_fitx, right_fitx, ploty = fit_line(binary_warped)
+
+	result = warp_pespective(undist, binary_warped, left_fitx, right_fitx, ploty, Minv)
+
+	return result
+
+if __name__=="__main__":
+
+	# Get lane binary image with color/gradient
+	'''
+	sample_image_path = ('/home/linfeng-zc/Documents/Udacity/CarND-Advanced-Lane-Lines/test_images/test6.jpg')
+	img = mpimg.imread(sample_image_path)
+	result = process_image(img)
+
+	f, axarr = plt.subplots(1, 2, figsize=(24, 9))
+	f.tight_layout()
+	axarr[0].imshow(result)
+	plt.show()
+	'''
+	white_output = 'project_video_result.mp4'
+	clip1 = VideoFileClip("project_video.mp4")
+	white_clip = clip1.fl_image(process_image) #NOTE: this function expects color images!!
+	white_clip.write_videofile(white_output, audio=False)
