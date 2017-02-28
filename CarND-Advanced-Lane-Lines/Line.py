bfcf907769335a64fabb249b655cb0740badf962
Chen Zhang <morning1319@gmail.com>
2017-02-28 11:07:01 -0800

    Update Line.py
    change function arguments

--- CarND-Advanced-Lane-Lines/Line.py
+++ CarND-Advanced-Lane-Lines/Line.py
@@ -25,21 +25,21 @@ class Line():
 		# y values for detected line pixels
 		self.ally = None
 
-	def calculate(self, ploty):
-		y_eval = np.max(ploty)
+	def calculate(self, x, y):
+		y_eval = np.max(y)
 
 		# Define conversions in x and y from pixels space to meters
 		ym_per_pix = 30/720 # meters per pixel in y dimension
 		xm_per_pix = 3.7/700 # meters per pixel in x dimension
 
 		# Fit new polynomials to x,y in world space
-		fit_cr = np.polyfit(ploty*ym_per_pix, self.currentx*xm_per_pix, 2)
+		fit_cr = np.polyfit(y*ym_per_pix, x*xm_per_pix, 2)
 
 		# Calculate the new radii of curvature
 		curverad = ((1 + (2*fit_cr[0]*y_eval*ym_per_pix + fit_cr[1])**2)**1.5) / np.absolute(2*fit_cr[0])
 
 		# Now our radius of curvature is in meters
 		print("Radius of Curvature: ", curverad, 'm')
-		# Example values: 632.1 m    626.2 m
 
+		# Example values: 632.1 m    626.2 m
 		self.radius_of_curvature = curverad
