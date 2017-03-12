942ffbfc93f983fff62e2f3511a5b019bce59605
Chen Zhang <morning1319@gmail.com>
2017-03-11 21:02:45 -0800

    vehicle detection first draft

---
+++ CarND-Vehicle-Detection/feature_extraction.py
@@ -0,0 +1,121 @@
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+from sklearn.preprocessing import StandardScaler
+from skimage.feature import hog
+
+# Define a function to compute binned color features  
+def bin_spatial(img, size=(32, 32)):
+	# Use cv2.resize().ravel() to create the feature vector
+	features = cv2.resize(img, size).ravel() 
+	# Return the feature vector
+	return features
+
+# Define a function to compute color histogram features  
+def color_hist(img, nbins=32, bins_range=(0, 256)):
+	# Compute the histogram of the color channels separately
+	channel1_hist = np.histogram(img[:,:,0], bins=nbins, range=bins_range)
+	channel2_hist = np.histogram(img[:,:,1], bins=nbins, range=bins_range)
+	channel3_hist = np.histogram(img[:,:,2], bins=nbins, range=bins_range)
+	# Concatenate the histograms into a single feature vector
+	hist_features = np.concatenate((channel1_hist[0], channel2_hist[0], channel3_hist[0]))
+	# Return the individual histograms, bin_centers and feature vector
+	return hist_features
+
+def get_hog_features(img, orient, pix_per_cell, cell_per_block, vis=False, feature_vec=True):
+	# Call with two outputs if vis==True
+	if vis == True:
+		features, hog_image = hog(img, orientations=orient, 
+					  pixels_per_cell=(pix_per_cell, pix_per_cell),
+					  cells_per_block=(cell_per_block, cell_per_block), 
+					  transform_sqrt=False, 
+					  visualise=vis, feature_vector=feature_vec)
+		return features, hog_image
+
+	# Otherwise call with one output
+	else:	  
+		features = hog(img, orientations=orient, 
+			   pixels_per_cell=(pix_per_cell, pix_per_cell),
+			   cells_per_block=(cell_per_block, cell_per_block), 
+			   transform_sqrt=False, 
+			   visualise=vis, feature_vector=feature_vec)
+		return features
+
+def convert_color(img, color_space):
+	if color_space != 'RGB':
+		if color_space == 'HSV':
+			feature_image = cv2.cvtColor(img, cv2.COLOR_RGB2HSV)
+			#feature_image = feature_image/255
+		elif color_space == 'LUV':
+			feature_image = cv2.cvtColor(img, cv2.COLOR_RGB2LUV)
+		elif color_space == 'HLS':
+			feature_image = cv2.cvtColor(img, cv2.COLOR_RGB2HLS)
+		elif color_space == 'YUV':
+			feature_image = cv2.cvtColor(img, cv2.COLOR_RGB2YUV)
+		elif color_space == 'YCrCb':
+			feature_image = cv2.cvtColor(img, cv2.COLOR_RGB2YCrCb)
+	else: 
+		feature_image = np.copy(img)
+
+	return feature_image	  
+
+def single_img_features(img, color_space='RGB', spatial_size=(32, 32),
+			hist_bins=32, orient=9, 
+			pix_per_cell=8, cell_per_block=2, hog_channel=0,
+			spatial_feat=True, hist_feat=True, hog_feat=True):	
+
+	img_features = []
+
+	feature_image = convert_color(img, color_space)
+
+	if spatial_feat == True:
+		spatial_features = bin_spatial(feature_image, size=spatial_size)
+		img_features.append(spatial_features)
+		#print("spatial_features: ", spatial_features.shape)
+
+	if hist_feat == True:
+		hist_features = color_hist(feature_image, nbins=hist_bins)
+		img_features.append(hist_features)
+		#print("color_features: ", hist_features.shape)
+
+	if hog_feat == True:
+		if hog_channel == 'ALL':
+			hog_features = []
+			for channel in range(feature_image.shape[2]):
+				hog_features.extend(get_hog_features(feature_image[:,:,channel], 
+									orient, pix_per_cell, cell_per_block, 
+									vis=False, feature_vec=True))	  
+		else:
+			hog_features = get_hog_features(feature_image[:,:,hog_channel], orient, 
+						pix_per_cell, cell_per_block, vis=False, feature_vec=True)
+
+		img_features.append(hog_features)
+
+		#print("hog_features: ", hog_features.shape)
+
+	return np.concatenate(img_features)
+
+def extract_features(imgs, color_space='RGB', spatial_size=(32, 32),
+			hist_bins=32, orient=9,
+			pix_per_cell=8, cell_per_block=2, hog_channel=0,
+			spatial_feat=True, hist_feat=True, hog_feat=True):
+
+	# Create a list to append feature vectors to
+	features = []
+	# Iterate through the list of images
+	for file in imgs:
+		image = mpimg.imread(file)
+		feature = single_img_features(image, color_space, spatial_size, hist_bins, orient,
+					pix_per_cell, cell_per_block, hog_channel, spatial_feat, hist_feat, hog_feat) 
+		
+		features.append(feature)
+	return features
+
+if __name__=="__main__":
+	filename = "/home/linfeng-zc/Documents/Udacity/CarND-Vehicle-Detection/vehicles/GTI_Far/image0000.png"
+	image = mpimg.imread(filename);
+
+	plt.imshow(image)
+	features = single_img_features(image, color_space='HSV')
+	print(features.shape)
---
+++ CarND-Vehicle-Detection/train.py
@@ -0,0 +1,101 @@
+import numpy as np
+import glob
+import random
+import time
+import pickle
+from feature_extraction import extract_features
+from sklearn.cross_validation import train_test_split
+from sklearn.svm import LinearSVC
+from sklearn.preprocessing import StandardScaler
+
+def train_svm_clf(car_file, notcar_file, test_size=0.2):
+	colorspace = 'RGB' # Can be RGB, HSV, LUV, HLS, YUV, YCrCb
+	orient = 9
+	pix_per_cell = 8
+	cell_per_block = 2
+	hog_channel = 'ALL' # Can be 0, 1, 2, or "ALL"
+	spatial_size=(32, 32)
+	hist_bins=32
+
+	t=time.time()
+	car_features = extract_features(car_file, color_space=colorspace, spatial_size=spatial_size, hist_bins=hist_bins,
+					orient=orient, pix_per_cell=pix_per_cell, cell_per_block=cell_per_block, 
+					hog_channel=hog_channel)
+
+	notcar_features = extract_features(notcar_file, color_space=colorspace, spatial_size=spatial_size, hist_bins=hist_bins,
+					orient=orient, pix_per_cell=pix_per_cell, cell_per_block=cell_per_block, 
+					hog_channel=hog_channel)
+	t2 = time.time()
+	print(round(t2-t, 2), 'Seconds to extract HOG features...')
+
+	# Create an array stack of feature vectors
+	X = np.vstack((car_features, notcar_features)).astype(np.float64)
+	# Normalize features
+	X_scaler = StandardScaler().fit(X)
+	scaled_X = X_scaler.transform(X)
+
+	# Define the labels vector
+	y = np.hstack((np.ones(len(car_features)), np.zeros(len(notcar_features))))
+
+	# Split up data into randomized training and test sets
+	rand_state = np.random.randint(0, 100)
+	X_train, X_test, y_train, y_test = train_test_split(scaled_X, y, test_size=test_size, random_state=rand_state)
+
+	print('Using:',orient,'orientations',pix_per_cell, 'pixels per cell and', cell_per_block,'cells per block')
+	print('Feature vector length:', len(X_train[0]))
+
+	# Use a linear SVC 
+	svc = LinearSVC()
+
+	# Check the training time for the SVC
+	t=time.time()
+	svc.fit(X_train, y_train)
+	t2 = time.time()
+	print(round(t2-t, 2), 'Seconds to train SVC...')
+
+	# Check the score of the SVC
+	print('Test Accuracy of SVC = ', round(svc.score(X_test, y_test), 4))
+
+	# Check the prediction time for a single sample
+	t=time.time()
+	n_predict = 10
+	print('My SVC predicts: ', svc.predict(X_test[0:n_predict]))
+	print('For these',n_predict, 'labels: ', y_test[0:n_predict])
+	t2 = time.time()
+	print(round(t2-t, 5), 'Seconds to predict', n_predict,'labels with SVC')
+	
+	svc_dict = {}
+	svc_dict["svc"] = svc
+	svc_dict["scaler"] = X_scaler
+	svc_dict["orient"] = orient
+	svc_dict["pix_per_cell"] = pix_per_cell
+	svc_dict["cell_per_block"] = cell_per_block
+	svc_dict["spatial_size"] = spatial_size
+	svc_dict["hist_bins"] = hist_bins
+	svc_dict["colorspace"] = colorspace
+
+	return svc_dict
+
+if __name__=="__main__":
+	car_path = '/home/linfeng-zc/Documents/Udacity/CarND-Vehicle-Detection/vehicles/GTI*/'
+	notcar_path = '/home/linfeng-zc/Documents/Udacity/CarND-Vehicle-Detection/non-vehicles/GTI/'
+
+	car_file = [file for file in glob.glob(car_path + '*.png', recursive=True)]
+	notcar_file = [file for file in glob.glob(notcar_path + '*.png', recursive=True)]
+	
+	input_file_len = len(car_file) if len(car_file) < len(notcar_file) else len(notcar_file)
+
+	random.shuffle(car_file)
+	random.shuffle(notcar_file)
+
+	
+	car_file = car_file[0:input_file_len]
+	notcar_file = notcar_file[0:input_file_len]
+
+	print ('CAR FILE NUMBER: ' , len(car_file))
+	print ('NOTCAR FILE NUMBER: ' , len(notcar_file))
+	svc_dict = train_svm_clf(car_file, notcar_file)
+	
+	with open('svc_pickle.p', 'wb') as f:
+		pickle.dump(svc_dict, f)
+
